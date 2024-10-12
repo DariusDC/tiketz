479e2324d5e3c1fadb5dd745edd428c1470db760
DariusCapolna <darius.capolna@rodeapps.com>
2024-10-12 11:32:27 +0300

    Move to kafka - wip

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,27 +1,32 @@
 import { OrderCreatedEvent, OrderStatus, Subjects } from "@dctiketz/common";
 import Listener from "@dctiketz/common/build/events/base-listener";
 import { queueGroupName } from "./queue-group-name";
-import { Message } from "node-nats-streaming";
 import { expirationQueue } from "../../queues/expiration-queue";
+import { EachMessagePayload } from "kafkajs";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
-  queueGroupName: string = queueGroupName;
 
-  async onMesssage(
-    data: OrderCreatedEvent["data"],
-    msg: Message
-  ): Promise<void> {
-    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log("Waiting... " + delay + "MS");
-
-    await expirationQueue.add(
-      { orderId: data.id },
-      {
-        delay,
-      }
-    );
-
-    msg.ack();
+  queueGroupName(): string {
+    return queueGroupName;
   }
+
+  onMesssage(data: OrderCreatedEvent["data"], msg: EachMessagePayload): void {}
+
+  // async onMesssage(
+  //   data: OrderCreatedEvent["data"],
+  //   msg:
+  // ): Promise<void> {
+  //   const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+  //   console.log("Waiting... " + delay + "MS");
+
+  //   await expirationQueue.add(
+  //     { orderId: data.id },
+  //     {
+  //       delay,
+  //     }
+  //   );
+
+  //   msg.ack();
+  // }
 }
